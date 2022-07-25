815d7f995ba7454da69a77775d30b3ea37236e50
amanmdesai <amanmukeshdesai@gmail.com>
2022-07-25 16:04:53 +0530

    add donwload,dataset

---
+++ dowload_samples.sh
@@ -0,0 +1,6 @@
+wget https://zenodo.org/record/5046389/files/background_for_training.h5?download=1
+wget https://zenodo.org/record/5055454/files/leptoquark_LOWMASS_lepFilter_13TeV.h5?download=1
+wget https://zenodo.org/record/5046446/files/Ato4l_lepFilter_13TeV.h5?download=1
+wget https://zenodo.org/record/5061633/files/hToTauTau_13TeV_PU20.h5?download=1
+wget https://zenodo.org/record/5061688/files/hChToTauNu_13TeV_PU20.h5?download=1
+wget https://zenodo.org/record/5070455/files/BlackBox_13TeV_PU20.h5?download=1
